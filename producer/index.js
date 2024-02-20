85fbe727fe2ab9e706c040ae8a99e2e970b02888
PerckyAts <percky@ucorp.io>
2024-02-20 15:08:50 +0300

    Send messages via endpoint

--- producer/index.js
+++ producer/index.js
@@ -1,35 +1,47 @@
-
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import Kafka from 'node-rdkafka';
 import eventType from '../eventType.js'; // Adjust the path based on your project structure
+
 const app = express();
 const port = 3000; // Adjust the port as needed
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json()); // Use JSON middleware instead of urlencoded
 app.use(express.static('public')); // Serve static files from the 'public' directory
+
+// Enable CORS for all routes
+app.use(cors());
+
 const stream = Kafka.Producer.createWriteStream({
-'metadata.broker.list': 'localhost:9092'
+  'metadata.broker.list': 'localhost:9092'
 }, {}, {
-topic: 'test'
+  topic: 'test'
 });
+
 stream.on('error', (err) => {
-console.error('Error in our Kafka stream');
-console.error(err);
+  console.error('Error in our Kafka stream');
+  console.error(err);
 });
+
 function queueMessage(message) {
-const event = { noise: message };
-const success = stream.write(eventType.toBuffer(event));
-if (success) {
-console.log(`Message queued: ${JSON.stringify(event.noise)}`);
-} else {
-console.log('Too many messages in the queue already..');
+  const event = { noise: message };
+  const success = stream.write(eventType.toBuffer(event));     
+  if (success) {
+    console.log(`Message queued: ${JSON.stringify(event.noise)}`);
+  } else {
+    console.log('Too many messages in the queue already..');
+  }
 }
-}
+
 app.post('/submit', (req, res) => {
-const message = req.body.message;
-queueMessage(message);
-res.send('Message sent successfully!');
+  console.log('submit on message queue');
+  const message = req.body.message; // Access the message from the request body
+  console.log('Message from front-end:', message);
+  queueMessage(message);
+  res.send('Message sent successfully!');
 });
+
 app.listen(port, () => {
-console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
