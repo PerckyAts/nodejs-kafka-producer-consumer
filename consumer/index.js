07ba7e3c1525433a46e1d16c0ef625bc50ecd03d
PerckyAts <percky@ucorp.io>
2024-03-08 10:51:27 +0300

    Consumer kafka public configuration

--- consumer/index.js
+++ consumer/index.js
@@ -1,16 +1,17 @@
-import express from 'express';
+// import express from 'express'; // Commented out because not used
 import http from 'http';
 import path from 'path';
 import Kafka from 'node-rdkafka';
-import cors from 'cors';
+import axios from 'axios'; // Import axios
 
-const app = express();
-const server = http.createServer(app);
+// Removed the unused express variable
+
+const server = http.createServer(); // Removed 'app' parameter
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const consumer = new Kafka.KafkaConsumer({
-  'group.id': 'kafka-percky',
+  'group.id': 'rogella',
   'metadata.broker.list': 'ntx-message-queue.hive404.com:9092',
 }, {});
 
@@ -24,68 +25,42 @@ consumer.on('ready', () => {
   consumer.subscribe(['response']);
   consumer.consume();
 }).on('data', function (data) {
-  const message = data.value.toString();
-  console.log(`Received message: ${message}`);
+
+  const message = JSON.parse(data.value.toString());
+  const id = message.id;
+  const winner = message.winner[0];
+  const accuracy = message.winner[1];
+
+  const results = {
+    "id": id,
+    "accuracy": accuracy,
+    "result": winner,
+    "new_status": true,
+  }
+
+  const dataToUpdate = {
+    "new_accurracy": accuracy,
+    "new_result": winner,
+    "new_status": true,
+  }
+
+  console.log("results:", results);
 
   messagesArray.push(message);
 
   if (responseStream) {
     responseStream.write(`data: ${message}\n\n`);
   }
+
+  // Send the results to the specified endpoint
+  axios.put(`http://0.0.0.0:8000/api/analize/update_analyze/${id}`, dataToUpdate)
+    .then(response => {
+      console.log(`Successfully updated analysis for ID ${id}`);
+    })
+    .catch(error => {
+      console.error(`Error updating analysis for ID ${id}:`, error.response.data.detail);
+    });
 });
-
-app.use(cors());
-
-app.use(express.static(__dirname));
-
-app.get('../', (req, res) => {
-  res.sendFile(path.join(__dirname, 'test.html'));
-});
-
-app.get('/events', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  responseStream = res;
-
-  req.on('close', () => {
-    responseStream = null;
-  });
-});
-
-app.get('/getMessages', (req, res) => {
-  res.json({ messages: messagesArray });
-});
-
-app.get('/submit', (req, res) => {
-  const message = req.query.message;
-  produceResponse({ noise: message });
-  res.send('Message sent successfully!');
-});
-
-function produceResponse(message) {
-  const producer = new Kafka.Producer({
-    'metadata.broker.list': 'ntx-message-queue.hive404.com:9092'
-  });
-
-  producer.connect();
-
-  producer.on('ready', () => {
-    producer.produce(
-      'response',
-      null,
-      Buffer.from(JSON.stringify(message)),
-      null,
-      Date.now()
-    );
-  });
-  
-
-  producer.on('event.error', (err) => {
-    console.error('Error from producer:', err);
-  });
-}
 
 const PORT = 3001;
 server.listen(PORT, () => {
